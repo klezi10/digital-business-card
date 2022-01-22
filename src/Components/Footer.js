979ee29e86fa6bbb9c03b2603815cafeda621c09
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-22 15:07:05 +0700

    styled icons and fixed border-radius around card

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="Footer">
-      <i className="fab fa-twitter-square"></i>
-      <i className="fab fa-github-square"></i>
+      <i className="fab fa-twitter-square twitter-icon"></i>
+      <i className="fab fa-github-square git-icon"></i>
     </div>
   );
 }
