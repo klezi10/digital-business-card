a28d21d771aea91210962ce6a62aa1c5c5afc3d8
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-22 14:28:50 +0700

    styled info component

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -7,14 +7,15 @@ export default function Info() {
       <img src={Avatar} alt="avatar" className="avatar" />
       <div className="personal-info">
         <h1>Kles Anderson</h1>
-        <p>Frontend Developer</p>
+        <p className="title">Frontend Developer</p>
         <a href="/" target="_blank">
-          klesand website
+          klesand.website
         </a>
         <button className="email-btn">
           <a href="mailto:">
-            <i className="fas fa-envelope"></i>
+            <i className="fas fa-envelope email-envelope"></i>
           </a>
+          Email
         </button>
       </div>
     </div>
